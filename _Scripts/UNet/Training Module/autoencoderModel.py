fa72639f022db440a8f33253ace4d9560e19faba
Zifeng Li <zil164@ucsd.edu>
2019-07-09 02:34:19 +0000

    autoencoder second test

--- _Scripts/UNet/Training Module/autoencoderModel.py
+++ _Scripts/UNet/Training Module/autoencoderModel.py
@@ -1,7 +1,12 @@
+import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
+from tensorflow.keras.utils import multi_gpu_model
+
+G = 2
+
 def autoencoder(input_size=(512, 512, 1)):
 
     input_img = Input(shape=input_size)  # adapt this if using `channels_first` image data format
@@ -31,7 +36,11 @@ def autoencoder(input_size=(512, 512, 1)):
     x = UpSampling2D((2, 2))(x)
     decoded = Conv2D(1, 1, activation='sigmoid', padding='same')(x)
 
-    model = Model(input_img, decoded)
+    with tf.device("/cpu:0"):
+        model = Model(input_img, decoded)
+
+    model = multi_gpu_model(model, gpus=G)
+
     model.compile(optimizer='adadelta', loss='binary_crossentropy')
     
     return model
