705c1fe16def8d512dc026005204ec5c91907d68
Zifeng Li <zil164@ucsd.edu>
2019-07-10 06:10:13 +0000

    another autoencoder test

--- _Scripts/UNet/Training Module/autoencoderData.py
+++ _Scripts/UNet/Training Module/autoencoderData.py
@@ -160,7 +160,8 @@ def labelVisualize(num_class,color_dict,img):
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
-        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
+        print(img)
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img*255)
 
 
 
