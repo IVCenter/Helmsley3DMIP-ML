ad4dd00893ca82084c8e36ff06a80311f1a77b05
Zifeng Li <zil164@ucsd.edu>
2019-07-22 23:29:35 +0000

    merge storage-a

--- _Scripts/UNet/Training Module/trainUnet.py
+++ _Scripts/UNet/Training Module/trainUnet.py
@@ -19,9 +19,9 @@ time_stamp = datetime.datetime.fromtimestamp(now).strftime('_%m_%d_%H_%M')
 This script train the model using the PNG images and labels, and save the model as hdf5 file
 '''
 
-image_folder = 'mri_images_30'
+image_folder = 'mri_images_30_autoencoder'
 label_folder = 'mri_labels_30'
-save_folder = 'model_checkpoint'
+save_folder = 'model_archive'
 model_name = 'colon'
 
 model_name = model_name + time_stamp
@@ -39,7 +39,7 @@ data_gen_args = dict(rotation_range=0.2,
 
 save_path = save_folder + '/' + model_name + '.hdf5'
 
-myGene = trainGenerator(2,'Datasets',image_folder,label_folder,data_gen_args,save_to_dir = None)
+myGene = trainGenerator(4,'Datasets',image_folder,label_folder,data_gen_args,save_to_dir = None)
 
 model = unet()
 # model = unet_lrelu()
